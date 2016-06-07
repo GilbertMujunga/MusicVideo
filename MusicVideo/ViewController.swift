4dab21e00f2d68822febde3fa62b2948c33d03ab
Gilbert Mujunga <gilbertmujunga@gilberts-air.fritz.box>
2016-06-07 13:49:20 +0200

    Completed part 2 coding session

--- MusicVideo/ViewController.swift
+++ MusicVideo/ViewController.swift
@@ -12,13 +12,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let api = APIManager()
+        api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json", completion: didLoadData)
+    }
+    func didLoadData(result:String){
+        let alert = UIAlertController(title: (result), message: nil , preferredStyle: .Alert)
+        let okAction  = UIAlertAction(title: "ok" , style: .Default) { action -> Void in
+         // do something you want
+        }
+        alert.addAction(okAction)
+        self.presentViewController(alert, animated: true, completion: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
 
 
 }
